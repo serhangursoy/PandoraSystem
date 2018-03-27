a4dd7bd8bb2d229c836a7fffafc6abdfffcc8982
Serhan Gürsoy <gursoyserhan@yandex.com>
2018-03-27 18:24:38 +0300

    Force push

--- src/system/SystemSocketConnectionHandler.js
+++ src/system/SystemSocketConnectionHandler.js
@@ -32,9 +32,9 @@ import {ServerActions} from "./ServerActions"
 export const SystemSocketConnectionHandler = function(callback){
     window.WebSocket = window.WebSocket || window.MozWebSocket;
 
-    //const connection = new WebSocket('ws://139.179.194.218:1337');
-    const connection = new WebSocket('ws://localhost:1337');
-
+    // const connection = new WebSocket('ws://139.179.194.218:1337');
+    //const connection = new WebSocket('ws://localhost:1337');
+    const connection = new WebSocket('ws://192.168.2.120:1337');
 
     connection.onopen = function () {
         console.log("Connection established");
